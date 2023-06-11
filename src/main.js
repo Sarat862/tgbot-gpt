0ad10e83d9a9dedffb44c30ab83d80131f1f4e48
Sarat862 <sarat862@gmail.com>
2023-06-11 22:03:33 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import { message } from "telegraf/filters";
 import { code } from "telegraf/format";
 import 'dotenv/config'
 
-// import { ogg } from "./ogg.js";
+import { ogg } from "./ogg.js";
 import { openai } from "./openai.js";
 
 const INITIAL_SESSION = {
@@ -24,28 +24,28 @@ bot.command("start", async (ctx) => {
     await ctx.reply("Чекаю Вашого голосового чи текстового повідомлення");
 })
 
-// bot.on(message("voice"), async (ctx) => {
-//     ctx.session = ctx.session ?? INITIAL_SESSION;
-//     try { 
-//         await ctx.reply(code("Повідомлення прийнято. Незабаром буде результат..."));
-//         const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id);
-//         const userId = String(ctx.message.from.id);
-//         const oggPath = await ogg.create(link.href, userId);
-//         const mp3Path = await ogg.toMP3(oggPath, userId);
+bot.on(message("voice"), async (ctx) => {
+    ctx.session = ctx.session ?? INITIAL_SESSION;
+    try { 
+        await ctx.reply(code("Повідомлення прийнято. Незабаром буде результат..."));
+        const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id);
+        const userId = String(ctx.message.from.id);
+        const oggPath = await ogg.create(link.href, userId);
+        const mp3Path = await ogg.toMP3(oggPath, userId);
 
-//         const text = await openai.transcription(mp3Path);
-//         await ctx.reply(code(`Ваше запитання: ${text}`));
+        const text = await openai.transcription(mp3Path);
+        await ctx.reply(code(`Ваше запитання: ${text}`));
 
-//         ctx.session.messages.push({ role: openai.roles.USER, content: text });
-//         const response = await openai.chat(ctx.session.messages);
-//         ctx.session.messages.push({ role: openai.roles.ASSISTANT, content: response.content });
+        ctx.session.messages.push({ role: openai.roles.USER, content: text });
+        const response = await openai.chat(ctx.session.messages);
+        ctx.session.messages.push({ role: openai.roles.ASSISTANT, content: response.content });
 
-//         await ctx.reply(response.content);
-//     }
-//     catch (e) {
-//         console.log("Error while error message", e.message);
-//     }
-// })
+        await ctx.reply(response.content);
+    }
+    catch (e) {
+        console.log("Error while error message", e.message);
+    }
+})
 
 bot.on(message("text"), async (ctx) => {
     ctx.session = ctx.session ?? INITIAL_SESSION;
